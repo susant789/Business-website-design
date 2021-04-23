a6d6b0ca63ab2b900abd001c8a845c3226d00138
susant789 <susantdasari@gmail.com>
2021-04-23 16:45:02 +0530

    completed navbar

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let menu = document.querySelector(".menu")
 menu.addEventListener("click",()=>{
     let navbar = document.querySelector(".navbar");
         navbar.classList.toggle("change");
+        menu.classList.toggle("change");
 })
